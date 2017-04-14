e9bb092e259ec097df994fd147d9b508de1277dc
Nikola Stojiljkovic <nikolavla@gmail.com>
2017-04-14 20:22:25 +0100

    Use Index to do the position lookup rather than the whole array

--- io/freezing/qlearning/state.py
+++ io/freezing/qlearning/state.py
@@ -2,10 +2,15 @@
 
 import numpy as np
 
-PLAYER_ARRAY = [0, 0, 0, 1]
-WALL_ARRAY = [0, 0, 1, 0]
-PIT_ARRAY = [0, 1, 0, 0]
-GOAL_ARRAY = [1, 0, 0, 0]
+PLAYER_IDX = 3
+WALL_IDX = 2
+PIT_IDX = 1
+GOAL_IDX = 0
+
+PLAYER_ARRAY = np.array([0, 0, 0, 1])
+WALL_ARRAY = np.array([0, 0, 1, 0])
+PIT_ARRAY = np.array([0, 1, 0, 0])
+GOAL_ARRAY = np.array([1, 0, 0, 0])
 
 class State(object):
     
@@ -15,31 +20,36 @@ class State(object):
         
         
     def __init_static(self):
-        self.state[0, 1] = np.array(PLAYER_ARRAY)
-        self.state[2, 2] = np.array(WALL_ARRAY)
-        self.state[1, 1] = np.array(PIT_ARRAY)
-        self.state[3, 3] = np.array(GOAL_ARRAY)
-        
-    def find_location(self, obj):
+        self.state[0, 1] = PLAYER_ARRAY
+        self.state[2, 2] = WALL_ARRAY
+        self.state[1, 1] = PIT_ARRAY
+        self.state[3, 3] = GOAL_ARRAY
+            
+    def __find_location(self, idx):
         for i in range(0, 4):
             for j in range(0, 4):
-                if (self.state[i,j] == obj).all():
+                if (self.state[i,j][idx] == 1):
                     return i,j
+        
+    def reward(self):
+        player_loc = self.__find_location(PLAYER_ARRAY)
+        
         
     def display_grid(self):
         grid = np.zeros((4, 4), dtype='<U2')
 
-        player_loc = self.find_location(PLAYER_ARRAY)
-        wall_loc = self.find_location(WALL_ARRAY)
-        pit_loc = self.find_location(PIT_ARRAY)
-        goal_loc = self.find_location(GOAL_ARRAY)
+        player_loc = self.__find_location(PLAYER_IDX)
+        wall_loc = self.__find_location(WALL_IDX)
+        pit_loc = self.__find_location(PIT_IDX)
+        goal_loc = self.__find_location(GOAL_IDX)
         
         if player_loc:
             grid[player_loc] = 'P'
             
         if wall_loc:
             grid[wall_loc] = 'W'
-            
+        
+        # TODO: Handle player is on Goal or Pit
         if pit_loc:
             grid[pit_loc] = '-'
                 
