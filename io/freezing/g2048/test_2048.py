d0f92b033e6aa6f71ea36a7d0687aa6a2f096213
Nikola Stojiljkovic <nikolavla@gmail.com>
2017-04-16 00:21:10 +0100

    Remove commented code

--- io/freezing/g2048/test_2048.py
+++ io/freezing/g2048/test_2048.py
@@ -4,15 +4,5 @@ from freezing.g2048.strategy_2048 import *
 from freezing.g2048.player_2048 import *
 
 strategy = min_preference_strategy
-game = Game2048(seed=1234)
-
+game = Game2048()
 play(strategy, game, verbose=True, allow_unavailable_actions=False)
-
-# print(s.show())
-# print()
-#
-# for action in range(NUMBER_OF_ACTIONS):
-#     s.do_action(action)
-#     print(ACTION_NAMES[action])
-#     print(s.show())
-#     print()
